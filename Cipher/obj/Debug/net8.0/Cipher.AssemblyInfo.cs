9237803b0621fd62d06dffad112a1d4b5aaf1beb
The-Lab-Exerpiment <akhilmudiyala@gmail.com>
2025-09-22 18:26:09 +0100

    ligma

--- Cipher/obj/Debug/net8.0/Cipher.AssemblyInfo.cs
+++ Cipher/obj/Debug/net8.0/Cipher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cipher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc7f5dbb9782845b23a231c36f81f12318fe9a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc59b4fb34aad5edc2d46d08575b5e45a3012d9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cipher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cipher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
