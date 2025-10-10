f14315235892b71a5bb09ab3779188239ac2c34b
The-Lab-Exerpiment <akhilmudiyala@gmail.com>
2025-10-10 19:27:38 +0100

    vector fitness

--- Python/main.py
+++ Python/main.py
@@ -80,3 +80,6 @@ while running:
         
     elif cmd == "chi fitness":
         print(st.variation(st.monolist(st.normalize_dict(td.get_mono_text(cipher))), st.monolist(st.normalize_dict(td.get_mono_frequencies("Data/mono_frequency.txt")))), '\n')
+        
+    elif cmd == "vector fitness":
+        print(st.vector_cos(st.monolist(st.normalize_dict(td.get_mono_text(cipher))), st.monolist(st.normalize_dict(td.get_mono_frequencies("Data/mono_frequency.txt")))), '\n')
